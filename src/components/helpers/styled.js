3b938f8ef074d602702fd2dfd7d3ce86efa415a4
Volodymyr Vitiuk <v.v.endeavour@gmail.com>
2023-10-12 00:46:03 +0300

    Create styled.js

---
+++ src/components/helpers/styled.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const SectionStyled = styled.section`
+  @media screen and (max-width: 428px) {
+    margin: 20px auto 40px;
+  }
+`;
+
+export const Container = styled.div`
+  @media screen and (max-width: 428px) {
+    margin-left: auto;
+    margin-right: auto;
+    padding-left: 15px;
+    padding-right: 15px;
+  }
+`;
