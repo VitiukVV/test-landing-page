8e818774eac5908c10e116e646726d8e84854114
Volodymyr Vitiuk <v.v.endeavour@gmail.com>
2023-10-12 00:45:52 +0300

    Create QuestionItem.styled.js

---
+++ src/components/FAQ/QuestionItem.styled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Item = styled.div`
+  border: 1px solid red;
+  border-radius: 28px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
