0d20578fae21a8de149bc8e1d03bfb098e10ec1b
Volodymyr Vitiuk <v.v.endeavour@gmail.com>
2023-10-12 00:45:26 +0300

    Create Brief.styled.js

---
+++ src/components/Brief/Brief.styled.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const BriefFormWrapper = styled.div`
+  border: 1px solid #e0e0e0;
+  border-radius: 8px;
+  padding: 20px;
+  text-align: center;
+`;
+
+export const Description = styled.p`
+  font-size: 16px;
+  margin-bottom: 20px;
+`;
