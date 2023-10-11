5a834e6fe87bbd68c4b5360463849e414463c1a2
Volodymyr Vitiuk <v.v.endeavour@gmail.com>
2023-10-12 00:46:53 +0300

    Create FirstBlock.styled.js

---
+++ src/components/TitleBlock/FirstBlock.styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const ImageWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+  justify-content: space-between;
+`;
+
+export const MainTitleWrapper = styled.div`
+  text-align: center;
+`;
