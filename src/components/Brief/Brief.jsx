ce418db96fb323af7b918ce356302c0e5e084b41
Volodymyr Vitiuk <v.v.endeavour@gmail.com>
2023-10-12 20:44:26 +0300

    Update Brief.jsx

--- src/components/Brief/Brief.jsx
+++ src/components/Brief/Brief.jsx
@@ -1,17 +1,22 @@
 import { OrderBtnStyled } from 'components/helpers/OrderBtn.styled';
 import React from 'react';
 import { BriefFormWrapper, Description } from './Brief.styled';
+import { Container, SectionStyled } from 'components/helpers/styled';
 
 const BriefFormBlock = () => {
   return (
-    <BriefFormWrapper>
-      <Description>
-        Бриф - це короткий документ, який допомагає нам краще зрозуміти ваші
-        потреби та вимоги. Будь ласка, заповніть бриф і дайте нам знати, як ми
-        можемо вам допомогти.
-      </Description>
-      <OrderBtnStyled>Заповнити зараз</OrderBtnStyled>
-    </BriefFormWrapper>
+    <SectionStyled>
+      <Container>
+        <BriefFormWrapper>
+          <Description>
+            Бриф - це короткий документ, який допомагає нам краще зрозуміти ваші
+            потреби та вимоги. Будь ласка, заповніть бриф і дайте нам знати, як
+            ми можемо вам допомогти.
+          </Description>
+          <OrderBtnStyled>Заповнити зараз</OrderBtnStyled>
+        </BriefFormWrapper>
+      </Container>
+    </SectionStyled>
   );
 };
 
