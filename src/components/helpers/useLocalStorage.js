58c047a4f1e3665a36a0f367e5090cffe0e3a538
Volodymyr Vitiuk <v.v.endeavour@gmail.com>
2023-10-12 20:30:37 +0300

    Update useLocalStorage.js

--- src/components/helpers/useLocalStorage.js
+++ src/components/helpers/useLocalStorage.js
@@ -1,8 +1,8 @@
 import { useEffect, useState } from 'react';
 
-export const useLocalStorage = (key, defaultValue) => {
+export const useLocalStorage = (key) => {
   const [state, setState] = useState(() => {
-    return JSON.parse(window.localStorage.getItem(key)) ?? defaultValue;
+    return JSON.parse(window.localStorage.getItem(key));
   });
 
   useEffect(() => {
