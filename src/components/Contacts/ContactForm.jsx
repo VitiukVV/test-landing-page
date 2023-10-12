d9b5f74088cb301fdddcad90f3010fd88620fc0d
Volodymyr Vitiuk <v.v.endeavour@gmail.com>
2023-10-12 20:36:51 +0300

    Update ContactForm.jsx

--- src/components/Contacts/ContactForm.jsx
+++ src/components/Contacts/ContactForm.jsx
@@ -75,11 +75,10 @@ const ContactForm = ({ onSubmitForm }) => {
         Email
         <Input
           onChange={handleChange}
-          value={number}
+          value={email}
           type="email"
           name="email"
-          pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
-          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
           required
         />
       </label>
